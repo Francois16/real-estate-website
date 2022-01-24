56cf5c3a62965529885e8899bcddbc2b346e5790
Francois <Fpaul1274@gmail.com>
2022-01-24 08:01:08 +0200

    Added content to services pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 # Views
-from .views import homeview
+from .views import homeview, property_portfolio_management, property_rentals
 
 urlpatterns = [
     path("", homeview, name="home"),
+    path("services/property-rentals/", property_rentals, name="rentals"),
+    path(
+        "services/property-portfolio-management/", property_portfolio_management, name="portfolio_management"
+    ),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,17 @@ def homeview(request):
     context = {}
 
     return render(request, template_name, context)
+
+
+def property_portfolio_management(request):
+    template_name = "pages/services/property_portfolio_management.html"
+    context = {}
+
+    return render(request, template_name, context)
+
+
+def property_rentals(request):
+    template_name = "pages/services/property_rentals.html"
+    context = {}
+
+    return render(request, template_name, context)
